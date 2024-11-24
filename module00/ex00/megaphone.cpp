e48e56dae007e4e34f06088424dbdac2641ce2f5
aaitelka <aaitelka@student.1337.ma>
2024-11-24 23:04:10 +0100

    megaphone modified, change type of len to size_t to match return of lenght function.

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -3,27 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaitelka <aaitelka@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: aaitelka <aaitelka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:28:28 by aaitelka          #+#    #+#             */
-/*   Updated: 2024/10/02 14:44:48 by aaitelka         ###   ########.fr       */
+/*   Updated: 2024/11/24 23:06:28 by aaitelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int main(int ac, char *av[]) {
-	
-	std::string	str;
-	
+
 	if (ac > 1) {
 		for (int i = 1; i < ac; i++) {
-			str = av[i];
-			for (int j = 0; j < str.length(); j++) {
+			std::string str = av[i];
+			size_t len = str.length();
+			for (size_t j = 0; j < len; j++) {
 				std::putchar(std::toupper(str[j]));
 			}
 		}
-	} else 
+	} else {		
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
 	std::cout << std::endl;
+	return 0;
 }
